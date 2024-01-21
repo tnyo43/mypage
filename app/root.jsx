53100e5fbd5b02918cfd921da0b192034b7bdd61
Tomoya Kashifuku <tnyo43@gmail.com>
2024-01-21 18:24:10 +0900

    use node server instead of remix-serve

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,4 @@
-import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
 
 export default function App() {
   return (
@@ -9,10 +9,11 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <h1>Hello world!bbb</h1>
+        <h1>Hello world!</h1>
         <Outlet />
 
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
